a9697184c50fbd12693a953a260a31d70c7d05a8
florianrhein <florian.rhein@pickware.de>
2022-09-29 15:13:44 +0200

    invalidate timer and add descriptions

--- TicTacToe/TicTacToe WatchKit Extension/View/GameViewModel.swift
+++ TicTacToe/TicTacToe WatchKit Extension/View/GameViewModel.swift
@@ -12,11 +12,13 @@ import Foundation
     private let gameService = GameService()
     private let playerToken: String
     private var subscribers = Set<AnyCancellable>()
+    private var timer: Timer? = nil
 
     @Published private var game: Game
     @Published internal private(set) var name: String = "unknown"
     @Published internal private(set) var boardValues: [String] = .init(repeating: "", count: 9)
-    @Published internal private(set) var gameState: String = ""
+    @Published internal private(set) var gameState: String = "" // TODO: should be formatted
+    @Published internal private(set) var isGameOver: Bool = false // TODO: should be set from the game state
 
     internal init(game: Game) {
         self.game = game
@@ -30,7 +32,18 @@ import Foundation
             }
             .store(in: &self.subscribers)
 
-        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.updateGame), userInfo: nil, repeats: true)
+        // Poll for game updates every two seconds
+        self.timer = Timer.scheduledTimer(
+            timeInterval: 2,
+            target: self,
+            selector: #selector(self.updateGame),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+
+    deinit {
+        self.timer?.invalidate()
     }
 
     internal func makeMove(atIndex index: Int) {
@@ -44,17 +57,26 @@ import Foundation
                         playerToken: self.playerToken,
                         nextMove: .init(nextMoveToken: nextMoveToken, field: index)
                     )
+            } catch let error {
+                print("An error occured while making a move: \(error)")
             }
         }
     }
 
+    /// A button is enabled when all of the following are true:
+    /// a) it is the current player's turn
+    /// b) the designated field is free
+    /// c) the game has a token for the next move
     internal func isButtonEnabled(forIndex index: Int) -> Bool {
-        self.gameState == "your_turn" && self.boardValues[index].isEmpty
+        self.game.state == "your_turn"
+        && self.game.board[index].lowercased() == "f"
+        && self.game.nextMoveToken?.isEmpty == false
     }
 
     @objc
     private func updateGame() {
         Task {
+            // use optional unwrap to ignore failed updates
             if let game = try? await self.gameService.loadGame(forPlayerToken: self.playerToken) {
                 self.game = game
             }
